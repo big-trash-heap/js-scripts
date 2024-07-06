6cd0de4aabb0a8c57389270ec13339f836a674f3
Кирилл Мидзяев <81816758+givename@users.noreply.github.com>
2024-07-07 02:41:09 +0500

    Create stringSeqsep.js

---
+++ stringSeqsep.js
@@ -0,0 +1,29 @@
+
+/**
+ * @param {string} str
+ * @param {number} gap
+ * @param {string} sep
+ * @param {boolean} [fromEnd=true]
+ */
+function stringSeqsep(str, gap, sep, fromEnd = true) {
+    const builded = [];
+    const gaps = Math.ceil(str.length / gap);
+    const offset = str.length % gap
+
+    if (fromEnd && offset !== 0) {
+        builded.push(str.slice(0, offset))
+
+        for (let i = 0; i < gaps - 1; ++i) {
+            const s = offset + i * gaps;
+            builded.push(str.slice(s, s + gap))
+        }
+    }
+    else {
+        for (let i = 0; i < gaps; ++i) {
+            const s = i * gaps;
+            builded.push(str.slice(s, s + gap))
+        }
+    }
+
+    return builded.join(sep);
+}
