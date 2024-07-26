3a8af82c87c5ecc9b6cecdeda3562f2d0ec5ef16
Кирилл Мидзяев <81816758+givename@users.noreply.github.com>
2024-07-26 11:42:34 +0500

    Update idempotency.mjs

--- idempotency.mjs
+++ idempotency.mjs
@@ -1,4 +1,24 @@
 /**
+ * Это позволяет на время исполнения замкнуть запрос с тем же ключём,
+ * это работает для асинхронных методов
+ *
+ * const getUser = ({ id }) => { return ... };
+ * const idempGetUser = idempotency(({ id }) => getUser({ id }), ({ id }) => id);
+ *
+ * const promise1 = idempGetUser({ id: 12 });
+ * const promise2 = idempGetUser({ id: 12 });
+ * const promise3 = idempGetUser({ id: 13 });
+ *
+ * console.log(promise1 === promise2); // true
+ * console.log(promise1 === promise3); // false
+ *
+ * await Promise.all([promise1, promise2, promise3]);
+ * const promise4 = idempGetUser({ id: 12 });
+ *
+ * console.log(promise1 === promise4); // false
+ */
+
+/**
  * @template { (...args: any) => any } T
  * @param { T } fn
  * @param { (...args: Parameters<T>) => string } idempotent
